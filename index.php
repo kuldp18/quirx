2c8964164ddab9cb8d4a8c970c3e0b1402b8d89b
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2023-12-30 18:23:59 +0530

    convert to .php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Quirx - Home Page</title>
+    <link rel="stylesheet" href="./css/global.css" />
+  </head>
+  <body>
+    <h1>Welcome to Quirx!</h1>
+    <p><a href="./pages/register.php">Register</a> here.</p>
+  </body>
+</html>
