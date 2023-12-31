0b51936eea6318f603313ab70dd4cf7d219b67a1
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2023-12-31 12:25:31 +0530

    style home page better

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Quirx - Home Page</title>
-    <link rel="stylesheet" href="./css/global.css" />
-  </head>
-  <body>
-    <h1>Welcome to Quirx!</h1>
-    <p><a href="./pages/register.php">Register</a> here.</p>
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Quirx - Home Page</title>
+  <link rel="stylesheet" href="./css/global.css" />
+  <link rel="stylesheet" href="./css/home.css" />
+</head>
+
+<body>
+  <main class="home">
+    <h1 class="home__title">Welcome to Quirx!</h1>
+    <a href="./pages/register.php" class="home__link">Register</a>
+    <a href="./pages/login.php" class="home__link">Login</a>
+  </main>
+
+</body>
+
 </html>
