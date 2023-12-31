e35019ce194b283d7a91efe2277cdd670a50ad31
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2023-12-31 15:33:37 +0530

    show logged in user's username to make sure we actually are logging in

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -52,7 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // set session variables
         $_SESSION["user_id"] = $result["id"];
         $_SESSION["user_username"] = htmlspecialchars($result["username"]);
-        $_SESSION["user_fullname"] = htmlspecialchars($result["fullname"]);
+        $_SESSION["user_fullname"] = htmlspecialchars($result["full_name"]);
         $_SESSION["last_regeneration"] = time();
 
         // redirect to home page
