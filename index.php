9d222d0f1838cab273ff05fb7f2270f12ea21fd1
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2024-04-04 20:08:43 +0530

    add basic string search

--- index.php
+++ index.php
@@ -23,16 +23,58 @@ require_once "./views/login.inc.php";
 
   <?php
   $videos = fetch_all_videos($pdo);
+  $search_results = [];
+  $search_query = "";
+
+  if (isset($_SESSION['search_results'])) {
+    $search_results = $_SESSION['search_results'];
+    $search_query = $_SESSION['search_query'];
+    unset($_SESSION['search_results']);
+    unset($_SESSION['search_query']);
+  }
   ?>
   <main class="videos">
     <section class="videos__search">
-      <form class="search_form">
-        <input type="text" name="search_form__input" class="search_form__input" placeholder="Search" required>
+      <form class="search_form" method="GET" action="./includes/search.inc.php">
+        <input type="text" name="search_query" class="search_form__input" placeholder="Search" required>
         <button type="submit" class="search_form__submit">
           <i class="fa-solid fa-magnifying-glass"></i>
         </button>
       </form>
     </section>
+
+    <!-- a similar section for search results -->
+    <?php if (!empty($search_results) && !empty($search_query)) : ?>
+      <section class="videos__search-results">
+        <h2 class="videos__title">Search results for "<?php echo $search_query; ?>"</h2>
+        <section class="video-list">
+          <?php foreach ($search_results as $video) : ?>
+            <a href="./pages/video_page.php?video_id=<?php echo $video['video_id']; ?>" class="video__link">
+              <article class="video">
+                <div class="video__thumbnail">
+                  <?php
+                  $thumbnail = $video['video_thumbnail'] ? "./uploads/thumbnails/" . $video['video_thumbnail'] : "https://placehold.co/1280x720/black/white?text=No+Thumbnail&font=monsterrat";
+                  ?>
+                  <img src="<?php echo $thumbnail; ?>" alt="No thumbnail found" class="video__thumbnail__img" width="300" height="200">
+                </div>
+                <div class="video__details">
+                  <h3 class="video__details__title"><?php echo $video['video_title']; ?></h3>
+                  <p class="video__details__user"><?php echo "@" . fetch_username_from_video_id($pdo, $video["video_id"]) ?></p>
+                </div>
+              </article>
+            </a>
+          <?php endforeach; ?>
+        </section>
+      </section>
+    <?php endif; ?>
+    <!-- if no results -->
+    <?php if (empty($search_results) && !empty($search_query)) : ?>
+      <section class="videos__search-results">
+        <h2 class="videos__title">No search results found for "<?php echo $search_query; ?>"</h2>
+      </section>
+    <?php endif; ?>
+
+
     <section class="videos__recent">
       <h2 class="videos__title">Recently uploaded</h2>
       <section class="video-list">
@@ -52,8 +94,6 @@ require_once "./views/login.inc.php";
             </article>
           </a>
         <?php endforeach; ?>
-
-
       </section>
     </section>
   </main>
---
+++ models/video_search.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+
+// search videos using search query
+function search_videos(object $pdo, string $search_query): array
+{
+    $query = "SELECT * FROM videos WHERE LOWER(video_title) LIKE LOWER(:search_query)";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute(['search_query' => "%$search_query%"]);
+    $videos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $videos;
+}
